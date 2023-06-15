38092bf90ffdfa1d6257a9a3129f22c3af9ecf70
CintraJP <jpmcintra@gmail.com>
2023-06-14 23:26:02 -0300

    estruturação de arquivos

---
+++ script.js
@@ -0,0 +1,45 @@
+const brunchs = [
+    {
+      name: "Waffle Morango",
+      price: 33.70,
+      description: "Delicie-se com nossos deliciosos waffles cobertos com generosas porções de morangos frescos. Cada mordida é uma explosão de sabor doce e suculento, combinado com a crocância irresistível do waffle. Uma opção perfeita para começar o seu dia ou simplesmente mimar-se em qualquer momento."
+    },
+    {
+      name: "Arroz Doce",
+      price: 22.50,
+      description: "Transporte-se para um sabor reconfortante com o nosso arroz doce especial. Feito com cuidado e carinho, cada colherada é uma experiência nostálgica, com o arroz cozido até ficar macio e cremoso, envolto em uma doce mistura de leite e aromas delicados. Uma sobremesa que aquece o coração e traz um sorriso ao seu rosto."
+    },
+    {
+      name: "Maça Doce",
+      price: 5.00,
+      description: "Nossa maçã doce é o toque perfeito de frescor e doçura para qualquer momento do dia. Cada mordida crocante revela a suculência da maçã, combinada com um leve toque de açúcar. Uma opção saudável e saborosa para satisfazer suas vontades, seja como um lanche rápido ou um acompanhamento delicioso."
+    },
+    {
+      name: "Crepe",
+      price: 35.90,
+      description: "Experimente a delicadeza dos nossos crepes artesanais. Com sua massa fina e macia, essas obras de arte culinárias são recheadas com uma variedade de opções irresistíveis. Seja doce ou salgado, cada crepe é uma combinação perfeita de texturas e sabores que irão satisfazer todos os seus desejos."
+    },
+    {
+      name: "Cookie Americano",
+      price: 8.70,
+      description: "Mergulhe em uma tradição americana com nossos cookies americanos clássicos. Com uma crosta dourada e uma textura macia por dentro, cada mordida revela pedaços generosos de chocolate. Desfrute desses cookies recém-saídos do forno, acompanhados de uma bebida quente, e permita-se saborear o conforto e o prazer de um doce indulgente."
+    },
+    {
+      name: "Cookie Dark",
+      price: 8.70,
+      description: "Descubra a perfeição do nosso cookie dark. Feito com chocolate amargo de alta qualidade, cada mordida é uma experiência intensa de sabor e sofisticação. A crocância equilibrada com a riqueza do chocolate irá satisfazer até mesmo os paladares mais exigentes. Desfrute de um momento de indulgência com esses cookies que são um verdadeiro deleite para os amantes de chocolate."
+    },
+    {
+      name: "Cookie Meio Amargo",
+      price: 8.70,
+      description: "Para os amantes de um sabor levemente amargo, nossos cookies meio amargo são a escolha ideal. Com a combinação perfeita de doçura e amargor, cada mordida oferece uma experiência equilibrada e única. Os pedaços de chocolate meio amargo derretem na boca, proporcionando uma explosão de sabores intensos e marcantes. Permita-se saborear esses cookies incríveis que vão além do convencional."
+    },
+  ]
+
+
+
+
+
+  for(let i = 0; i < brunchs.length; i++){
+    console.log(brunchs[i])
+  }
